847fe83b9342ed6001842c38cbf3bfe51badcb94
marsel01kg <mbekbolotov945@gmail.com>
2025-04-28 17:11:06 +0600

    fixing

--- fairy_tail_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FairyTailApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fairy_tail_api'
--- fairy_tail_api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from fairy_tail_api.models import Character
-
-
-class CharacterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Character
-        fields = '__all__'
