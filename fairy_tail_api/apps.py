f453395b347ea484005b23f7d509345c4e9d615c
marsel01kg <mbekbolotov945@gmail.com>
2025-04-28 16:55:51 +0600

    api

---
+++ fairy_tail_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FairyTailApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'fairy_tail_api'
---
+++ fairy_tail_api/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
+
+from fairy_tail_api.models import Character
+from fairy_tail_api.serializers import CharacterSerializer
+
+
+class CharacterView(ListAPIView):
+    queryset = Character.objects.all()
+    serializer_class = CharacterSerializer
+# Create your views here.
